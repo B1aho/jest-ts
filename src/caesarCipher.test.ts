831a702fe0e79c5d6dce2133b099ef0edf2bf8f5
B1aho <wheelsurgut00@gmail.com>
2025-03-23 17:01:26 +0300

    Write implementation that pass basic cases

--- src/caesarCipher.test.ts
+++ src/caesarCipher.test.ts
@@ -6,6 +6,10 @@ describe("Base test cases", () => {
     });
 
     it("Basic cipher #1", () => {
-        expect(caesarCipher("xyz", 3)).toBe("abc")
+        expect(caesarCipher("xyz", 3)).toBe("abc");
+    });
+
+    it("Basic cipher #2", () => {
+        expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
     });
 });
--- src/caesarCipher.ts
+++ src/caesarCipher.ts
@@ -1,3 +1,20 @@
 export function caesarCipher(str: string, offset: number): string {
-    return "";
+    let strArray = Array.from(str);
+    offset = offset % 26;
+    strArray = strArray.map((letter, idx) => {
+        let charCode = str.charCodeAt(idx) + offset;
+        if (letter >= 'a' && letter <= 'z') {
+            if (charCode > 122)
+                charCode -= 26;
+            else if (charCode < 97)
+                charCode += 26;
+        } else if (letter >= 'A' && letter <= 'Z') {
+            if (charCode > 90)
+                charCode -= 26;
+            else if (charCode < 65)
+                charCode += 26;
+        }
+        return String.fromCharCode(charCode);
+    });
+    return strArray.join('');
 }
