30ed8ca363b2a71df20da3c35851838306fb7be3
B1aho <wheelsurgut00@gmail.com>
2025-03-23 08:50:43 +0300

    Refactor code, reduce duplication

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,13 +1,16 @@
-type binaryOperator = (x: number | bigint, y: number | bigint) => number | bigint
+type BinaryOperator = (x: number | bigint, y: number | bigint) => number | bigint;
+type Operator = '+' | '-' | '*' | '/';
+type NumberAll = bigint | number;
 
 interface ICalculator {
-    add: binaryOperator;
-    substract: binaryOperator;
-    divide: binaryOperator;
-    multiple: binaryOperator;
-    pow: (x: number | bigint, n: number) => number | bigint;
+    add: BinaryOperator;
+    substract: BinaryOperator;
+    divide: BinaryOperator;
+    multiple: BinaryOperator;
+    pow: (x: NumberAll, n: number) => NumberAll;
     isFloat: (x: number) => boolean;
-    isBigInt: (x: number | bigint) => boolean;
+    isBigInt: (x: NumberAll) => boolean;
+    bigintHelper: (x: NumberAll, y: NumberAll, operator: Operator) => NumberAll;
 }
 
 export const calculator: ICalculator = {
@@ -19,51 +22,57 @@ export const calculator: ICalculator = {
         return typeof x === 'bigint';
     },
 
-    add(x, y) {
-        if (this.isBigInt(x) || this.isBigInt(y)) {
-            if (isNaN(Number(x)) || isNaN(Number(y))) return NaN;
-            if (!isFinite(Number(x)) || !isFinite(Number(y))) {
-                return !isFinite(Number(x)) ? x : y;
-            }
-            return (BigInt(x) + BigInt(y));
+    bigintHelper(x, y, operator) {
+        if (isNaN(Number(x)) || isNaN(Number(y))) return NaN;
+        if (!isFinite(Number(x)) || !isFinite(Number(y))) {
+            return !isFinite(Number(x))
+                ? x
+                : operator === '-'
+                    ? -y
+                    : y;
         }
+        switch (operator) {
+            case "*":
+                return (BigInt(x) * BigInt(y));
+            case "/":
+                if (y === 0n || y === 0) return NaN;
+                return (BigInt(x) % BigInt(y) === 0n)
+                    ? BigInt(x) / BigInt(y)
+                    : NaN;
+            case "+":
+                return (BigInt(x) + BigInt(y));
+            case "-":
+                return (BigInt(x) - BigInt(y));
+        }
+    },
+
+    add(x, y) {
+        if (this.isBigInt(x) || this.isBigInt(y))
+            return this.bigintHelper(x, y, "+");
+
         let sum = Number(x) + Number(y);
         return this.isFloat(sum) ? parseFloat(sum.toFixed(10)) : sum;
     },
 
     substract(x, y) {
-        if (this.isBigInt(x) || this.isBigInt(y)) {
-            if (isNaN(Number(x)) || isNaN(Number(y))) return NaN;
-            if (!isFinite(Number(x)) || !isFinite(Number(y))) {
-                return !isFinite(Number(x)) ? x : -y;
-            }
-            return (BigInt(x) - BigInt(y));
-        }
+        if (this.isBigInt(x) || this.isBigInt(y))
+            return this.bigintHelper(x, y, "-");
         let substraction = Number(x) - Number(y);
         return this.isFloat(substraction) ? parseFloat(substraction.toFixed(10)) : substraction;
     },
 
     multiple(x, y) {
-        if (this.isBigInt(x) || this.isBigInt(y)) {
-            if (isNaN(Number(x)) || isNaN(Number(y))) return NaN;
-            if (!isFinite(Number(x)) || !isFinite(Number(y))) {
-                return !isFinite(Number(x)) ? x : y;
-            }
-            return (BigInt(x) * BigInt(y));
-        }
+        if (this.isBigInt(x) || this.isBigInt(y))
+            return this.bigintHelper(x, y, "*");
+
         let multiplication = Number(x) * Number(y);
         return this.isFloat(multiplication) ? parseFloat(multiplication.toFixed(10)) : multiplication;
     },
 
     divide(x, y) {
-        if (this.isBigInt(x) || this.isBigInt(y)) {
-            if (isNaN(Number(x)) || isNaN(Number(y))) return NaN;
-            if (!isFinite(Number(x)) || !isFinite(Number(y))) {
-                return !isFinite(Number(x)) ? x : y;
-            }
-            if (y === 0n || y === 0) return NaN;
-            return (BigInt(x) % BigInt(y) === 0n) ? BigInt(x) / BigInt(y) : NaN;
-        }
+        if (this.isBigInt(x) || this.isBigInt(y))
+            return this.bigintHelper(x, y, "/");
+
         let division = Number(x) / Number(y);
         return this.isFloat(division) ? parseFloat(division.toFixed(10)) : division;
     },
