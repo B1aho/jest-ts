a9051e10845a61903123945362352ba545527dca
B1aho <wheelsurgut00@gmail.com>
2025-03-23 06:59:33 +0300

    Write and pass cases for adding

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -21,3 +21,63 @@ describe("All needed operators exist:", () => {
         expect(typeof calculator.pow).toBe("function");
     });
 });
+
+describe("Test cases for add operator", () => {
+    it("Basic adding #1", () => {
+        expect(calculator.add(10, 5)).toBe(15);
+    });
+
+    it("Basic adding #2", () => {
+        expect(calculator.add(101, 2000)).toBe(2101);
+    });
+
+    it("Add float number", () => {
+        expect(calculator.add(0.00123, 0.1234)).toBe(0.12463);
+    });
+
+    it("Add with NaN", () => {
+        expect(calculator.add(NaN, 123)).toBe(NaN);
+    });
+
+    it("Add with infinite values #1", () => {
+        expect(calculator.add(Infinity, 123)).toBe(Infinity);
+    });
+
+    it("Add with infinite values #2", () => {
+        expect(calculator.add(-Infinity, 123)).toBe(-Infinity);
+    });
+
+    it("Add with infinite values #3", () => {
+        expect(calculator.add(-Infinity, Infinity)).toBe(NaN);
+    });
+
+    it("Add with bigint values #1", () => {
+        expect(calculator
+            .add(1122121323213123213312312321n, 1232132132213123123112312312n))
+            .toBe(2354253455426246336424624633n);
+    });
+
+    it("Add with bigint mix number #1", () => {
+        expect(calculator
+            .add(9007199254740991n, 10))
+            .toBe(9007199254741001n);
+    });
+
+    it("Add with bigint mix number #2", () => {
+        expect(calculator
+            .add(9007199254741001n, 10))
+            .toBe(9007199254741011n);
+    });
+
+    it("Add with bigint mix number infinite", () => {
+        expect(calculator
+            .add(9007199254741001n, Infinity))
+            .toBe(Infinity);
+    });
+
+    it("Add with bigint mix NaN", () => {
+        expect(calculator
+            .add(9007199254741001n, NaN))
+            .toBe(NaN);
+    });
+});
--- src/calculator.ts
+++ src/calculator.ts
@@ -1,4 +1,4 @@
-type binaryOperator = (x: number, y: number) => number
+type binaryOperator = (x: number | bigint, y: number | bigint) => number | bigint
 
 interface ICalculator {
     add: binaryOperator;
@@ -6,26 +6,44 @@ interface ICalculator {
     divide: binaryOperator;
     multiple: binaryOperator;
     pow: binaryOperator;
+    isFloat: (x: number) => boolean;
+    isBigInt: (x: number | bigint) => boolean;
 }
 
 export const calculator: ICalculator = {
+    isFloat(x: number): boolean {
+        return (isFinite(x) && !Number.isInteger(x))
+    },
+
+    isBigInt(x: number | bigint): boolean {
+        return typeof x === 'bigint';
+    },
+
     add(x, y) {
+        if (this.isBigInt(x) || this.isBigInt(y)) {
+            if (isNaN(Number(x)) || isNaN(Number(y))) return NaN;
+            if (!isFinite(Number(x)) || !isFinite(Number(y))) {
+                return !isFinite(Number(x)) ? x : y;
+            }
+            return (BigInt(x) + BigInt(y));
+        }
+        let sum = Number(x) + Number(y);
+        return this.isFloat(sum) ? parseFloat(sum.toFixed(10)) : sum;
+    },
+
+    substract(x, y) {
         return 0;
     },
 
-    substract() {
+    divide(x, y) {
         return 0;
     },
 
-    divide() {
+    multiple(x, y) {
         return 0;
     },
 
-    multiple() {
-        return 0;
-    },
-
-    pow() {
+    pow(x, n) {
         return 0;
     },
 
